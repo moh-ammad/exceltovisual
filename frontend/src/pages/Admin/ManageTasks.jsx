b570fbb4dc2225021520f549f68f0f212fd4e35f
moh-ammad <mohammadsohail9392@gmail.com>
2025-08-19 11:21:40 +0530

    changed instance again

--- frontend/src/pages/Admin/ManageTasks.jsx
+++ frontend/src/pages/Admin/ManageTasks.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
-import { API_ENDPOINTS } from '@/utils/apisPaths.js';
+import { API_ENDPOINTS } from '@/utils/apisPaths';
 import toast from 'react-hot-toast';
 import { Trash2, Pencil } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 import ConfirmationPopup from '@/createtasks/ConfirmationPopUp';
-import { showError, showSuccess } from '@/utils/helper.js';
-import axiosInstance from '@/utils/axiosInstance.js';
+import { showError, showSuccess } from '@/utils/helper';
+import axiosInstance from '@/utils/axiosInstance';
 
 const ManageTasks = () => {
   const [tasks, setTasks] = useState([]);
