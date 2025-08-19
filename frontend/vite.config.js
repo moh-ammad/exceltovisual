c114822a9eb302e6583812e60c38505af1912f55
moh-ammad <mohammadsohail9392@gmail.com>
2025-08-19 15:41:38 +0530

    made changes to vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-// https://vitejs.dev/config/
+// __dirname replacement
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
-    extensions: ['.js', '.jsx', '.json'], // ✅ add this
+    extensions: ['.js', '.jsx', '.json'], // optional
   },
   server: {
     port: 3000,
