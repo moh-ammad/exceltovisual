c5ad41a41cdd2b0e8dcacd511cf9379f5fbc4242
moh-ammad <mohammadsohail9392@gmail.com>
2025-08-19 19:37:48 +0530

    again chnaged vite config

--- frontend/src/components/layouts/AuthLayout.jsx
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -4,7 +4,7 @@ const AuthLayout = ({ children }) => {
   return (
     <div className="flex pb-4">
       <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
-        <h2 className="text-lg font-medium text-black">Task Manager</h2>
+        <h2 className="text-lg font-medium text-white">Task Manager</h2>
         {children}
       </div>
       <div
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
 import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 
-// __dirname replacement
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': resolve(__dirname, 'src'),
     },
-    extensions: ['.js', '.jsx', '.json'], // optional
+    extensions: ['.js', '.jsx', '.json'],
   },
   server: {
     port: 3000,
