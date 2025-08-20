e3f78f4f2ca078b287f08832a35276ff9c5a6ba8
moh-ammad <mohammadsohail9392@gmail.com>
2025-08-20 14:05:39 +0530

    added domain to backend

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['https://exceltovisual.netlify.app', 'http://localhost:3000'],
     credentials: true,
     methods:['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
