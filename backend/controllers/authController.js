a12eaf9c0ef3d99418204a9f3839487920fe3bb1
Anjumunnisa939 <anjumrpt@gmail.com>
2025-08-21 10:13:24 +0530

    final commit

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -192,7 +192,7 @@ const updateUserProfile = async (req, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
