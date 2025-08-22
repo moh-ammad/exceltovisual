3ff5716e70f7788f427d725093910631c30a89e2
moh-ammad <mohammadsohail9392@gmail.com>
2025-08-22 09:49:02 +0530

    added preflight option for cors in backends server.js

--- backend/server.js
+++ backend/server.js
@@ -26,18 +26,24 @@ const app = express()
 // ]
 
 // CORS middleware
+// origin: (origin, callback) => {
+//   if (!origin || allowedOrigins.includes(origin)) {
+//     callback(null, true)
+//   } else {
+//     callback(new Error('Not allowed by CORS'))
+//   }
+// },
+// CORS middleware
 app.use(cors({
-  // origin: (origin, callback) => {
-  //   if (!origin || allowedOrigins.includes(origin)) {
-  //     callback(null, true)
-  //   } else {
-  //     callback(new Error('Not allowed by CORS'))
-  //   }
-  // },
-  origin:process.env.CLIENT_URL||"*",
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  origin: process.env.CLIENT_URL || '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
-}))
+}));
+
+// Preflight handler
+app.options('*', cors());
+
 
 if (process.env.NODE_ENV === 'production') {
   app.use(helmet()) // Full protection in production
